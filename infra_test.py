320672cb3497430bdb2874083de063ab12489505
Eldio Santos <eldio.santos@m4u.com.br>
2020-03-21 18:16:02 -0300

    Changed some print statements

--- infra_test.py
+++ infra_test.py
@@ -6,16 +6,15 @@ import pytest
 class TestRequirements(TestCase):
 
     def setUp(self):
-        print("My test setup...")
         super(TestRequirements, self).setUp()
 
     def test_app_script(self):
-        print("hello.py file")
+        print("test_app_script")
         env_file = self.host.file('/www/hello.py')
         env_file.exists
 
     def test_app_html_template(self):
-        print("hello.py file")
+        print("test_app_html_template")
         env_file = self.host.file('/www/index.html')
         env_file.exists
 
@@ -28,5 +27,5 @@ class TestRequirements(TestCase):
         assert len(processes) == 1
 
     def test_app_is_listening_in_port_80(self):
-        print("test_process_running")
+        print("test_app_is_listening_in_port_80")
         assert self.host.socket("tcp://0.0.0.0:80").is_listening
